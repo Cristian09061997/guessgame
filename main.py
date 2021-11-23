a8e318fc79681674941cd09ccb41bf4d22cd65e3
cristian <cristian09061997@gmail.com>
2021-11-23 14:02:56 -0800

    actualizacion 2da actividad

--- main.py
+++ main.py
@@ -17,3 +17,30 @@ def humanguess():
             print(f"El numero que escogio es menor {numero}")
 
 humanguess()
+
+def computerGuess():
+    max = 1000
+    min = 0
+    number = random.randrange(min,max) #es el de la computadora
+    bandera = False 
+    
+    while(bandera == False):
+        print (number)
+        print("Este es tu numero?")
+        opc= input("Si | No:  ").lower()
+        if( opc == "no"):
+            print("Tu numero es mayor o menor al mio?")
+            opc2 = input("Mayor | Menor: ").lower()
+            if (opc2 == "menor"): #En caso que es menor
+                max = number-1
+                newNumber = random.randrange(min,max) 
+                number = newNumber
+            else: #En caso que es mayor
+                min = number+1
+                newNumber = random.randrange(min,max)
+                number = newNumber
+        else:
+            bandera = True
+
+    
+computerGuess()
